279d01de5136de855f0dfb4dbf90e6e23e6a4962
Juan Lorente Guarnieri <juanlorenteguarnieri@gmail.com>
2024-02-12 00:52:38 +0100

    Added class structure and subclases door gate, modificated lemming animation structure

--- src/lemmings/Lemmings_game.hpp
+++ src/lemmings/Lemmings_game.hpp
@@ -8,6 +8,8 @@
 #include "Lemmings_camera.hpp"
 #include "Lemming.hpp"
 #include "Level_manager.hpp"
+#include "Door.hpp"
+#include "Gate.hpp"
 
 // Incluir otros archivos necesarios como Lemming, LevelManager, etc.
 
@@ -21,7 +23,6 @@ private:
   // Otros miembros privados como LevelManager, UI/HUD, etc.
 
 public:
-
   Lemmings_game()
       : Game("Lemmings")
   {
@@ -42,11 +43,15 @@ public:
     auto map1 = std::make_shared<Entity>(Point3f(0, 0.6, 1), Vector2f(0.1, 0.25), t2, "MAP");
     auto map2 = std::make_shared<Entity>(Point3f(0.9, 0.6, 1), Vector2f(0.1, 0.25), t2, "MAP");
     auto map3 = std::make_shared<Entity>(Point3f(0.45, 0.6, 1), Vector2f(0.1, 0.25), t2, "MAP");
+    auto door = std::make_shared<Door>(Point3f(0.2, 0.5, 1), Vector2f(0.082, 0.052), engine, 1, 1.0f, 15, 1.0f);
+    auto gate = std::make_shared<Gate>(Point3f(0.8, 0.65, 1), Vector2f(0.088, 0.104), engine, 1, 0.8f);
 
     create_entity(map);
     create_entity(map1);
     create_entity(map2);
     create_entity(map3);
+    create_entity(door);
+    create_entity(gate);
   }
 
   void on_loop_start([[maybe_unused]] Engine &engine) override
