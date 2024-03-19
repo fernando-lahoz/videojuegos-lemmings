f858c791f7e4c69534b218c050040b9d5ea331d2
hsunekichi <hsunekichi@gmail.com>
2024-03-19 13:10:51 +0100

    Added ttl to particles

--- include/engine/particle.hpp
+++ include/engine/particle.hpp
@@ -3,13 +3,34 @@
 class Particle : public Entity
 {
 
-    Particle(Point3f position, Vector2f diagonal, const Texture& texture, 
+protected:
+    Float time_to_live;
+
+public:
+
+    Particle(Point3f position, Vector2f diagonal,
+            Float time_to_live, // in seconds
+            const Texture& texture, 
             std::string_view _entity_name, 
             std::string_view _class_name="Particle")
         :
         Entity(position, diagonal, texture, _entity_name, _class_name)
     {
+        this->time_to_live = time_to_live;
         constructor_set_collision_type(Entity::Collision_type::HUD);
+    }
+
+
+    void pre_physics(Engine& engine) override
+    {
+        Entity::pre_physics(engine);
+
+        time_to_live -= engine.get_delta_time();
+
+        if (time_to_live <= 0)
+        {
+            destroy();
+        }
     }
 
 };
