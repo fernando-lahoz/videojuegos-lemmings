2a86156736fece0e598dd4ffaeb87968199d0675
hsunekichi <hsunekichi@gmail.com>
2024-04-21 13:00:16 +0200

    Added tile generator script

---
+++ tile_generator.py
@@ -0,0 +1,19 @@
+import cv2
+
+
+
+# Load image
+img = cv2.imread('map1.png')
+
+n_rows = 5
+n_cols = 10
+
+# Draw a grid
+for i in range(1, n_rows):
+    cv2.line(img, (0, i * img.shape[0] // n_rows), (img.shape[1], i * img.shape[0] // n_rows), (0, 0, 0), 2)
+for i in range(1, n_cols):
+    cv2.line(img, (i * img.shape[1] // n_cols, 0), (i * img.shape[1] // n_cols, img.shape[0]), (0, 0, 0), 2)
+    
+# Print image
+cv2.imshow('image', img)
+cv2.waitKey(0)
