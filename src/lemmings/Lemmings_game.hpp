b1329bd64795cde59c2d4170a052daea3832496e
Juan Lorente Guarnieri <juanlorenteguarnieri@gmail.com>
2024-05-04 21:07:02 +0200

    PROPUESTA 02 done, added wait cursor

--- src/lemmings/Lemmings_game.hpp
+++ src/lemmings/Lemmings_game.hpp
@@ -88,7 +88,7 @@ public:
 
     auto cursor = std::make_shared<Cursor>(engine, game_info, 25);
     create_entity(cursor);
-    auto cursor_menu = std::make_shared<Cursor_menu>(engine, game_info, 24);
+    auto cursor_menu = std::make_shared<Cursor_menu>(engine, game_info);
     std::cout << cursor_menu << std::endl;
     create_entity(cursor_menu);
     game_info.set_cursor_menu(cursor_menu);
--- src/lemmings/cursor/Cursor_menu.hpp
+++ src/lemmings/cursor/Cursor_menu.hpp
@@ -14,17 +14,39 @@ private:
   bool state = false;
   Game_info &game_info;
   float size;
+  bool in_transition = false;
+  float timer_rand_wait = 0.0f;
 
 public:
-  Cursor_menu(Engine &engine, Game_info &_game_info, float _size)
-      : Rigid_body(Point3f(-1, -1, -30), Vector2f(_size, _size * 1.04166666666), engine.load_texture("assets/cursor_menu.png"), 
-        engine, "Cursor"), game_info(_game_info), size(_size)
+  Cursor_menu(Engine &engine, Game_info &_game_info)
+      : Rigid_body(Point3f(-1, -1, -30), Vector2f(64, 64), engine.load_texture("assets/cursor_menu.png"),
+                   engine, "Cursor"),
+        game_info(_game_info), size(64)
   {
     constructor_set_collision_type(Entity::Collision_type::CURSOR);
   }
 
   void update_state(Engine &engine) override
   {
+    if (!game_info.get_can_do_transition() && !in_transition)
+    {
+      in_transition = true;
+      timer_rand_wait = (Float)(rand() % 400) / 1000 + 0.2f;
+    }
+    if (game_info.get_can_do_transition() && in_transition)
+    {
+      in_transition = false;
+    }
+    if (timer_rand_wait > 0)
+    {
+      timer_rand_wait -= engine.get_delta_time();
+      set_active_texture(engine.load_texture("assets/cursor_menu_wait.png"));
+    }
+    else
+    {
+      set_active_texture(engine.load_texture("assets/cursor_menu.png"));
+    }
+
     if (game_info.get_actual_state() == Utils::STATE::MENU)
     {
       Point2f pos = engine.get_mouse_position();
