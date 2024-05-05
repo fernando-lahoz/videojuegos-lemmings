faca22becaac7891aa1e4cdb5465de4537f4126b
Juan Lorente Guarnieri <juanlorenteguarnieri@gmail.com>
2024-05-05 13:53:13 +0200

    fixed bug LB057, change explosion range

--- src/lemmings/Lemming.hpp
+++ src/lemmings/Lemming.hpp
@@ -843,11 +843,12 @@ public:
         }
         if (is_exploding())
         {
-          Bound2f box = Bound2f(Point2f(local_to_world(Point2f(0.35, 0))), Point2f(local_to_world(Point2f(0.65, 1))));
-          Bound2f box2 = Bound2f(Point2f(local_to_world(Point2f(0.65, 0))), Point2f(local_to_world(Point2f(0.35, 1))));
-          Bound2f box3 = Bound2f(Point2f(local_to_world(Point2f(0.2, 0.05))), Point2f(local_to_world(Point2f(0.8, 0.95))));
-          Bound2f box4 = Bound2f(Point2f(local_to_world(Point2f(0.05, 0.2))), Point2f(local_to_world(Point2f(0.95, 0.8))));
-          Bound2f box5 = Bound2f(Point2f(local_to_world(Point2f(0.15, 0.15))), Point2f(local_to_world(Point2f(0.85, 0.85))));
+          Bound2f box = Bound2f(Point2f(local_to_world(Point2f(0.35, 0.05))), Point2f(local_to_world(Point2f(0.65, 1.15))));
+          Bound2f box2 = Bound2f(Point2f(local_to_world(Point2f(0.3, 0.1))), Point2f(local_to_world(Point2f(0.7, 0.5))));
+          Bound2f box3 = Bound2f(Point2f(local_to_world(Point2f(0.25, 0.15))), Point2f(local_to_world(Point2f(0.75, 1.1))));
+          Bound2f box4 = Bound2f(Point2f(local_to_world(Point2f(0.2, 0.25))), Point2f(local_to_world(Point2f(0.8, 1.05))));
+          Bound2f box5 = Bound2f(Point2f(local_to_world(Point2f(0.15, 0.35))), Point2f(local_to_world(Point2f(0.85, 1))));
+          Bound2f box6 = Bound2f(Point2f(local_to_world(Point2f(0.1, 0.5))), Point2f(local_to_world(Point2f(0.9, 0.85))));
 
           bool destroyed = false;
           auto &entities = engine.get_entities();
@@ -865,6 +866,8 @@ public:
                 destroyed = true;
               if (entity->destroy_box_alpha(engine, box5))
                 destroyed = true;
+              if (entity->destroy_box_alpha(engine, box6))
+                destroyed = true;
             }
             else if (entity->get_entity_name() == "DIRECTIONAL WALL")
             {
@@ -878,6 +881,8 @@ public:
               if (dir_wall_ptr->destroy_box_alpha(engine, box4, 0))
                 destroyed = true;
               if (dir_wall_ptr->destroy_box_alpha(engine, box5, 0))
+                destroyed = true;
+              if (dir_wall_ptr->destroy_box_alpha(engine, box6, 0))
                 destroyed = true;
             }
           }
